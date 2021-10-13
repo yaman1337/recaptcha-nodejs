ccd0f4eee2e207887106caa0fbb891cb456be2a9
= <=>
2021-10-13 09:42:12 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const request = require('request')
+const { PORT, KEY } = require('./secret')
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/subscribe', (req,res) => {
+    const { captcha } = req.body
+    if(captcha === '' || captcha === undefined || captcha === null) {
+        return res.json({success: false, msg: "Invalid recaptcha"})
+    }
+
+    const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${KEY}&response=${captcha}&remoteip=${req.connection.remoteAddress}`
+
+    request(verifyUrl, (err, response, body) => {
+        body = JSON.parse(body)
+        if(body.success !== undefined && !body.success) {
+            return res.json({success: false, msg: "Failed captcha verification."})
+        }
+        
+        res.json({success: true, msg: "You have been subscribed."})
+
+    })
+
+})
+
+app.listen(PORT, (err) => {
+    if(err) throw err
+    console.log(`Running on port ${PORT}`)
+})
---
+++ secret.js
@@ -0,0 +1,9 @@
+/*
+    The keys are for development environment. Replace it with your own keys to use in production
+    environment.
+*/
+
+module.exports = {
+    PORT: 9000,
+    KEY: '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe' //Use your keys here.
+}
